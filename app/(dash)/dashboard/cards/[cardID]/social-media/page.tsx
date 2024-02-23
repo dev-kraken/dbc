6f747d313a72cf731da9415ac16950e783efbb60
Dev Kraken <somanbandesha.sb@gmail.com>
2024-02-24 02:06:23 +0400

    Drag and Drop Install

---
+++ app/(dash)/dashboard/cards/[cardID]/social-media/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CardSocialMediaList } from '@/components/cards/social-media/card-social-media-list'
+
+const SocialMedia = () => {
+  return (
+    <div>
+      <CardSocialMediaList />
+    </div>
+  )
+}
+
+export default SocialMedia
